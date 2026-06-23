3a5133915bbacec77937729238849cfc6679afb9
Digvijay Patil <dypatil2007@gmail.com>
2026-06-24 00:18:04 +0530

    Time: 24 ms (12.13%), Space: 45.8 MB (70.84%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(),people.end());
-        int lo=0;
-        int hi=people.size()-1;
-        int count=0;
-        while(lo<=hi){
-            if((people[lo] + people[hi]) <= limit){
+        sort(people.begin(), people.end());
+        int lo = 0;
+        int hi = people.size() - 1;
+        int count = 0;
+        while (lo <= hi) {
+            if ((people[lo] + people[hi]) <= limit) {
                 lo++;
             }
             hi--;
             count++;
-
         }
         return count;
     }
