0f13f612647fa38ba8d46fca0b5e74e3abff0576
Digvijay Patil <dypatil2007@gmail.com>
2026-06-27 23:53:34 +0530

    Time: 16 ms (96.82%), Space: 23.1 MB (97.04%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    // Recursive function to find all increasing subsequences
-    void solve(vector<int> nums, int index, vector<int> output, set<vector<int>>& ans){
-        // Base case: if we have reached the end of the input array
-        if(index>=nums.size()){
-            // Only insert into ans if output has more than one element
-            if(output.size()>1)
-                ans.insert(output);
+    void solve(int idx , int previ , vector<int>& nums ,vector<int>& temp, vector<vector<int>>& ans){
+        if(idx==nums.size()){
+            if(temp.size()>1)
+                ans.push_back(temp);
             return ;
         }
-        // If output is empty or current element is greater than or equal to the last element in output
-        if(output.size()==0 || nums[index] >= output.back()){
-            output.push_back(nums[index]);
-            solve(nums, index+1, output, ans);
-            output.pop_back();
-        }
-        solve(nums, index+1, output, ans);
-    }
 
+        if(previ==-1 || nums[idx]>=nums[previ]){
+            temp.push_back(nums[idx]);
+            solve(idx+1, idx,nums,temp , ans);
+            temp.pop_back();
+        }
+        if(previ==-1 || nums[idx]!=nums[previ])
+        solve(idx+1 , previ , nums , temp , ans );
+    }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
-        // Initialize output and set to store all increasing subsequences
-        vector<int> output;
-        set<vector<int>> ans;
-        // Call recursive function to find all increasing subsequences
-        solve(nums, 0, output, ans);
-        // Return ans as a vector
-        return vector(ans.begin(), ans.end());
+        int n = nums.size() ;
+        vector<vector<int>> ans;
+        vector<int> temp ;
+        solve(0,-1,nums,temp , ans);
+        vector<vector<int>> res(ans.begin(),ans.end());
+        return res;
     }
 };
